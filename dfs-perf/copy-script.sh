98230d6d04713c4c26edee56b46cb2c5ca76a723
saukad <saukad@node0.saukad-qv80848.redundancy-pg0.apt.emulab.net>
2020-09-22 13:08:04 -0600

    Added command to copy public key to DFS worker nodes

--- dfs-perf/copy-script.sh
+++ dfs-perf/copy-script.sh
@@ -5,5 +5,6 @@ PROJ=$3
 for number in {22..41}
 do
 scp node-setup.sh node$number.$USER-$EXP.$PROJ.apt.emulab.net:~/
+scp ~/.ssh/id_rsa.pub node$number.$USER-$EXP.$PROJ.apt.emulab.net:~/.ssh/
 done
 exit 0
